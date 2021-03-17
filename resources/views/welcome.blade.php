146451dace44fb92eabb1095d23f4d33358c6cfb
badhon86 <badhon35-2092@diu.edu.bd>
2021-03-17 23:01:34 +0600

    student.update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laracasts  thousands of video tutorial   JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
+                                    Laracasts  thousands of video tutoria   JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
                                 </div>
                             </div>
                         </div>
